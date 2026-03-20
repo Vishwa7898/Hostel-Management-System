5b781212f9b58561b89c7926e2ab7f73505529a7
Sammani <sammanisubhasinghe997@gmail.com>
2026-03-20 14:01:19 +0530

    update

---
+++ Backend/models/Room.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+  roomNumber: { type: String, required: true },
+  type: { type: String, required: true },
+  floor: { type: Number, required: true },
+  totalBeds: { type: Number, required: true },
+  price: { type: Number, required: true },
+  image: { type: String },
+  // මේ fields අලුතින් එක් කරන්න
+  occupiedBeds: { type: Number, default: 0 }, 
+  occupiedCount: { type: Number, default: 0 },
+  assignedStudentName: { type: String, default: "None" },
+  status: { type: String, default: "Available" }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Room', roomSchema);
